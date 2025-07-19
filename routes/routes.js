67718d3e1f34d2859f8d55c20006daaf65d75f9f
kartik <149373290+ikartikbhatt@users.noreply.github.com>
2025-07-20 01:46:45 +0530

    project completed

--- routes/routes.js
+++ routes/routes.js
@@ -26,4 +26,12 @@ router.delete("/deleteFile", (req, res) => {
   utils.deleteFile(req, res);
 });
 
+//no routes
+
+router.use((req, res, next) => {
+  res.status(404).json({
+    message: "Route not found"
+  });
+});
+
 module.exports = router;
